b38e9897caeba2eb20034b8e2405e3651448f3b0
pedroLSoares <pedro.levada@mercadolivre.com>
2022-03-29 10:43:41 -0300

    initial commit - unit test exercises

---
+++ src/test/java/com/meli/obterdiploma/StudentDAOTests.java
@@ -0,0 +1,163 @@
+package com.meli.obterdiploma;
+
+import com.meli.obterdiploma.exception.StudentNotFoundException;
+import com.meli.obterdiploma.model.StudentDTO;
+import com.meli.obterdiploma.model.SubjectDTO;
+import com.meli.obterdiploma.repository.StudentDAO;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StudentDAOTests {
+
+    private static StudentDAO studentDAO;
+    private StudentDTO mockedStudent;
+
+    @BeforeAll
+    public static void beforeAll(){
+        studentDAO = new StudentDAO();
+    }
+
+    @BeforeEach
+    public void beforeEach(){
+        studentDAO.resetData();
+        StudentDTO mockedStudent = new StudentDTO(
+                1L,
+                "Mocked student",
+                "",
+                5.0,
+                List.of(new SubjectDTO(
+                        "Mock subject",
+                        5.5
+                ))
+        );
+        this.mockedStudent = mockedStudent;
+
+        studentDAO.save(mockedStudent);
+    }
+
+    @AfterAll
+    public static void afterAll(){
+        studentDAO.resetData();
+    }
+
+    // SAVE
+    @Test
+    public void shouldSaveNewStudent(){
+        StudentDTO mockedStudent = new StudentDTO(
+                111L,
+                "Mocked student",
+                "",
+                5.0,
+                List.of(new SubjectDTO(
+                        "Mock subject",
+                        5.5
+                ))
+        );
+
+        studentDAO.save(mockedStudent);
+        boolean exists = studentDAO.exists(mockedStudent);
+
+        assertTrue(exists);
+
+    }
+
+    @Test
+    public void shouldSaveNewWithoutID(){
+        StudentDTO mockedStudent = new StudentDTO(
+                null,
+                "Mocked student",
+                "",
+                5.0,
+                List.of(new SubjectDTO(
+                        "Mock subject",
+                        5.5
+                ))
+        );
+
+        studentDAO.save(mockedStudent);
+        boolean exists = studentDAO.exists(mockedStudent);
+
+        assertTrue(exists);
+
+    }
+
+    @Test
+    public void shouldNotSaveNullValue(){
+        assertThrows(NullPointerException.class, () -> studentDAO.save(null));
+
+    }
+
+    // DELETE
+    @Test
+    public void shouldDeleteStudent(){
+        boolean removed = studentDAO.delete(1L);
+
+        assertTrue(removed);
+    }
+
+    @Test
+    public void shouldNotDeleteStudentWhenPassInvalidID(){
+        boolean removed = studentDAO.delete(null);
+
+        assertFalse(removed);
+    }
+
+    @Test
+    public void shouldNotDeleteStudentWhenReceiveNonExistentID(){
+        boolean removed = studentDAO.delete(9999L);
+
+        assertFalse(removed);
+    }
+
+    // EXISTS
+    @Test
+    public void shouldCheckIfStudentExists(){
+
+        assertTrue(studentDAO.exists(this.mockedStudent));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenNotFindStudent(){
+        assertFalse(studentDAO.exists(new StudentDTO(
+                999L,
+                "Non existent",
+                "",
+                0.0,
+                new ArrayList<>())));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenCheckNullValue(){
+        assertThrows(NullPointerException.class, () -> studentDAO.exists(null));
+    }
+
+
+    // FIND_BY_ID
+    @Test
+    public void shouldFindStudentByID(){
+        StudentDTO studentDTO = studentDAO.findById(1L);
+
+        assertNotNull(studentDTO);
+    }
+
+    @Test
+    public void shouldNotFindStudent(){
+        assertThrows(StudentNotFoundException.class, () -> studentDAO.findById(999L));
+    }
+
+    @Test
+    public void shouldNotFindStudentWithNullID(){
+        assertThrows(StudentNotFoundException.class, () -> studentDAO.findById(null));
+    }
+
+
+
+
+}
---
+++ src/test/java/com/meli/obterdiploma/StudentServiceTests.java
@@ -0,0 +1,129 @@
+package com.meli.obterdiploma;
+
+
+import com.meli.obterdiploma.exception.StudentNotFoundException;
+import com.meli.obterdiploma.model.StudentDTO;
+import com.meli.obterdiploma.model.SubjectDTO;
+import com.meli.obterdiploma.repository.IStudentDAO;
+import com.meli.obterdiploma.repository.IStudentRepository;
+import com.meli.obterdiploma.repository.StudentDAO;
+import com.meli.obterdiploma.repository.StudentRepository;
+import com.meli.obterdiploma.service.StudentService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StudentServiceTests {
+
+    private IStudentDAO mockStudentDao;
+
+    private IStudentRepository mockStudentRpository;
+
+    private StudentService service;
+
+    private final StudentDTO mockedStudent = new StudentDTO(
+            111L,
+            "Mocked student",
+            "",
+            5.0,
+            List.of(new SubjectDTO(
+                    "Mock subject",
+                    5.5
+            ))
+    );
+
+    @BeforeEach
+    public void beforeEach(){
+        this.mockStudentDao = Mockito.mock(StudentDAO.class);
+        this.mockStudentRpository = Mockito.mock(StudentRepository.class);
+
+        this.service = new StudentService(this.mockStudentDao, this.mockStudentRpository);
+    }
+
+    // CREATE
+    @Test
+    public void shouldCreateStudent(){
+
+
+        this.service.create(mockedStudent);
+        Mockito.verify(mockStudentDao, Mockito.times(1)).save(this.mockedStudent);
+    }
+
+    @Test
+    public void shouldCallSaveWhenReceiveNull(){
+        this.service.create(null);
+        Mockito.verify(mockStudentDao, Mockito.times(1)).save(null);
+    }
+
+    //READ
+
+    @Test
+    public void shouldFindStudent(){
+        Mockito.when(mockStudentDao.findById(1L)).thenReturn(mockedStudent);
+
+        StudentDTO foundStudent = service.read(1L);
+
+        assertEquals(mockedStudent, foundStudent);
+        Mockito.verify(mockStudentDao, Mockito.times(1)).findById(1L);
+    }
+
+    @Test
+    public void shouldReceiveExceptionIFNotFoundStudent(){
+        Long id = 99L;
+        Mockito.when(mockStudentDao.findById(id)).thenThrow(new StudentNotFoundException(id));
+
+        assertThrows(StudentNotFoundException.class, () -> service.read(id));
+        Mockito.verify(mockStudentDao, Mockito.times(1)).findById(id);
+    }
+
+    @Test
+    public void shouldReceiveExceptionWhenReceiveNullID(){
+        Long id = null;
+        Mockito.when(mockStudentDao.findById(id)).thenThrow(new StudentNotFoundException(id));
+
+        assertThrows(StudentNotFoundException.class, () -> service.read(id));
+        Mockito.verify(mockStudentDao, Mockito.times(1)).findById(id);
+    }
+
+    // UPDATE
+
+    @Test
+    public void shouldUpdateStudent(){
+        service.update(mockedStudent);
+        Mockito.verify(mockStudentDao, Mockito.times(1)).save(mockedStudent);
+    }
+
+    // DELETE
+    @Test
+    public void shouldDeleteStudent(){
+
+        service.delete(1L);
+
+        Mockito.verify(mockStudentDao, Mockito.times(1)).delete(1L);
+    }
+
+    @Test
+    public void shouldCallDeleteWithNullID(){
+        service.delete(null);
+        Mockito.verify(mockStudentDao, Mockito.times(1)).delete(null);
+    }
+
+    // GET ALL
+
+    @Test
+    public void shouldFindAllStudents(){
+        Mockito.when(mockStudentRpository.findAll()).thenReturn(Set.of(mockedStudent));
+
+        Set<StudentDTO> students = service.getAll();
+
+        assertEquals(Set.of(mockedStudent).size(), students.size());
+        Mockito.verify(mockStudentRpository, Mockito.times(1)).findAll();
+    }
+
+
+}
